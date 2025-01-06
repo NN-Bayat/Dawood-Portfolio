2a24ad2de5ec2a816eebbbbd606eae73b218c3a9
NN-Bayat <noorullahbayat1414@gmail.com>
2025-01-06 12:27:03 +0430

    js update

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+const navLinks = document.querySelectorAll("header nav a");
+const logoLink = document.querySelector(".logo");
+const sections = document.querySelectorAll("section");
+
+const activePage = () => {
+    const header = document.querySelector("header");
+    const barsBox = document.querySelector(".bars_box");
+
+    header.classList.remove("active");
+    setTimeout(() => {
+        header.classList.add("active");
+    }, 1100);
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+    });
+
+    barsBox.classList.remove("active");
+    setTimeout(() => {
+        barsBox.classList.add("active");
+    }, 1100);
+
+    sections.forEach(section => {
+        section.classList.remove("active");
+    });
+};
+
+navLinks.forEach((link, inx) => {
+    link.addEventListener('click', () => {
+        if (!link.classList.contains("active")) {
+            activePage();
+
+            link.classList.add("active");
+
+            setTimeout(() => {
+                sections[inx].classList.add("active")
+            }, 1100);
+        };
+    });
+});
+
+logoLink.addEventListener("click", () => {
+    if (!navLinks[0].classList.contains("active")) {
+        activePage();
+
+        navLinks[0].classList.add("active");
+
+        setTimeout(() => {
+            sections[0].classList.add("active")
+        }, 1100);
+    }
+});
+
 const resumeBtns = document.querySelectorAll(".resume_btn");
 
 resumeBtns.forEach((btn, inx) => {
@@ -16,4 +69,44 @@ resumeBtns.forEach((btn, inx) => {
     });
 });
 
+const arrowRight = document.querySelector(".portfolio_box .navigation .arrow_right");
+const arrowLeft = document.querySelector(".portfolio_box .navigation .arrow_left");
+
+let index = 0;
+
+const activePortolio = () => {
+    const imgSlide = document.querySelector(".portfolio_carousel .img_slide");
+    const porfolioDetails = document.querySelectorAll(".portfolio_detail");
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+    porfolioDetails.forEach(detail => {
+        detail.classList.remove("active");
+    });
+    porfolioDetails[index].classList.add("active");
+};
+
+arrowRight.addEventListener('click', () => {
+    if (index < 4) {
+        index++;
+        arrowLeft.classList.remove("disabled");
+    } else {
+        index = 5;
+        arrowRight.classList.add("disabled");
+    }
+
+    activePortolio();
+});
+
+arrowLeft.addEventListener('click', () => {
+    if (index > 1) {
+        index--;
+        arrowRight.classList.remove("disabled");
+    } else {
+        index = 0;
+        arrowLeft.classList.add("disabled");
+    }
+
+    activePortolio();
+});
 
